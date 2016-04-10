090e9d7c7d9f5e6bddb58c9b4f5769219d814956
Joe Dalton <joe@sequence.com>
2016-04-10 16:33:51 -0700

    Added meta tags, and scroll-to-top btn

--- prototype/src/js/main.js
+++ prototype/src/js/main.js
@@ -3,6 +3,7 @@ $(function(){
   // UTILITY
   //------------------
   var $body = $('body');
+  var $window = $(window);
 
   function isMobile() {
     return (window.innerWidth < 768);
@@ -14,40 +15,20 @@ $(function(){
   var header = (function(){
 
     var $header = $('header');
-    var $window = $(window);
     var $mobileBtn = $header.find('.mobile-btn');
-    var stickThreshold = 500;
-    var desktopHeight = 252;
-    var isPlaceholder = false;
 
-    // function addPlaceholder() {
-    //   var placeholderEl = '<div class="placeholder"></div>';
-    //   $body.prepend(placeholderEl);
-    //   isPlaceholder = true;
-    // }
-    // function removePlaceholder() {
-    //   var $placeholder = $('.placeholder');
-    //   $placeholder.remove();
-    //   isPlaceholder = false;
-    // }
+    var headerHeight = $header.height() * 2;
+    var showHeight = $window.height();
 
     function watchTop() {
       $window.scroll(function(){
         var position = $window.scrollTop();
-        // var belowThresh = (position > stickThreshold);
 
-        if (position > stickThreshold) {
-          $body.addClass('sticky');
+        window.requestAnimationFrame(function(){
+          $body.toggleClass('sticky', (position > headerHeight));
+          $header.toggleClass('show', (position > showHeight));
+        });
 
-          // if (!isPlaceholder) {
-          //   addPlaceholder();
-          // }
-        } else {
-          // if (isPlaceholder) {
-          //   removePlaceholder();
-          // }
-          $body.removeClass('sticky');
-        }
       });
     }
     function bindMobile() {
@@ -57,8 +38,13 @@ $(function(){
     }
 
     function init(){
-      watchTop();
-      bindMobile();
+
+      if ( isMobile() ) {
+        bindMobile();
+      } else {
+        watchTop();
+      }
+
     }
 
     return {
@@ -69,5 +55,82 @@ $(function(){
   header.init();
 
 
+  var homeScroll = (function(){
+    var $backLetters = $('.back-letter');
+    var midScreen = $window.height() * .7;
+
+    function hideBackLetters() {
+      $backLetters.addClass('hide');
+    }
+    function showLetters(position) {
+      $window.scroll(function(){
+        var position = $window.scrollTop();
+        var showSpot = position + midScreen;
+
+        // if at the top, re-hide the letters
+        if (!position) {
+          hideBackLetters();
+          return;
+        }
+
+        // loop through letters and show the ones in view
+        for (var i = 0; i < $backLetters.length; i++) {
+          var $el = $backLetters.eq(i);
+
+          if ($el.offset().top < showSpot) {
+            $el.removeClass('hide');
+
+          } else {
+            break;
+          }
+        }
+
+      });
+        
+    }
+    function init() {
+      hideBackLetters();
+      showLetters();
+    }
+
+    return {
+      init: init
+    };
+  })();
+
+  homeScroll.init();
+
+
+  var toTopBtn = (function(){
+    var $topBtn = $('.to-top-btn');
+    var showSpot = $window.height();
+
+    function scrollWatch() {
+      $window.scroll(function(){
+        var position = $window.scrollTop();
+
+        $topBtn.toggleClass('show', (position > showSpot) );
+      });
+    }
+    function bindClick() {
+      $topBtn.click(function(){
+        $body.animate({
+          scrollTop: 0
+        }, 600);
+      });
+
+    }
+
+    function init() {
+      bindClick();
+      scrollWatch();
+    }
+
+    return {
+      init: init
+    }
+  })();
+
+  toTopBtn.init();
 
 });
